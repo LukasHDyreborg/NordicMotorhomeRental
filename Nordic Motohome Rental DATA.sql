f49a94c872852c66478cdc2280156c36f994ce53
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-05-31 19:35:11 +0200

    Ændrede også lige info i data filen til dansk

--- Nordic Motohome Rental DATA.sql
+++ Nordic Motohome Rental DATA.sql
@@ -9,8 +9,8 @@ INSERT INTO customers() VALUES ('1', 'Steve Nielsen', '35', 'Stevesmail@gmail.co
                                 awning BOOLEAN);
 INSERT INTO economy() VALUES (licensePlate, gasBurners);*/
 
-INSERT INTO motorhomes() VALUES ('BG 23 432', 'Citroen', '2CV', 1000, 4, 2, 'Benzin', 'Manual', 100000,
-                                 '2017-02-02', 'L: 4m H: 2m', 'Economy', true, true, true);
+INSERT INTO motorhomes() VALUES ('BG 23 432', 'Citroen', '2CV', 1000, 4, 2, 'Benzin', 'Manuel', 100000,
+                                 '2017-02-02', 'L: 4m H: 2m', 'Økonomi', true, true, true);
 INSERT INTO economy() VALUES ('BG 23 432', 3);
 
 -- Standard
@@ -19,7 +19,7 @@ INSERT INTO economy() VALUES ('BG 23 432', 3);
                                 awning BOOLEAN);
 INSERT INTO standard() VALUES (licensePlate, shower, elStove);*/
 
-INSERT INTO motorhomes() VALUES ('LD 12 345', 'Ford', 'AutoModel', 1500, 6, 4, 'Diesel', 'Automatic', 150000,
+INSERT INTO motorhomes() VALUES ('LD 12 345', 'Ford', 'AutoModel', 1500, 6, 4, 'Diesel', 'Automat', 150000,
                                  '2019-02-02', 'L: 4m H: 2m', 'Standard', true, true, true);
 INSERT INTO standard() VALUES ('LD 12 345', true, true);
 
@@ -30,8 +30,8 @@ INSERT INTO standard() VALUES ('LD 12 345', true, true);
 INSERT INTO standard() VALUES (licensePlate, shower, elStove);
 INSERT INTO standard() VALUES (licensePlate, tv, rearViewCamera);*/
 
-INSERT INTO motorhomes() VALUES ('CN MS 123', 'Tesla', 'Model 5', 2500, 8, 6, 'Diesel', 'Automatic', 75000,
-                                 '2019-12-02', 'L: 3,75m H: 2,5m', 'Luxury', true, true, true);
+INSERT INTO motorhomes() VALUES ('CN MS 123', 'Tesla', 'Model 5', 2500, 8, 6, 'Diesel', 'Automat', 75000,
+                                 '2019-12-02', 'L: 3,75m H: 2,5m', 'Luksus', true, true, true);
 INSERT INTO standard() VALUES ('CN MS 123', true, true);
 INSERT INTO luxury() VALUES ('CN MS 123', true, true);
 
